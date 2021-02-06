7ec2b5d8f9d9a5164ad035b7dbf72dfbf3132531
Lesha-Kartoshka <lesha.liho@gmail.com>
2021-02-06 14:58:48 +0300

    feat: reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0){
+      n = -n
+    }
+    number = n;
+    var stringifyed = n.toString();
+    var length = stringifyed.length;
+    var reversed = 0;
+    for(var i = 0; i < length; i++){
+      var buffer = number%10;
+      number = Math.floor(number / 10);
+      reversed = reversed * 10 + buffer;
+    }
+    return reversed;
 }
